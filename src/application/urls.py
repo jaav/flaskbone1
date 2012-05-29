c9cf8467a936a43e8b99a4a9334b1fa41267f171
Krishna Palakaluri <krishnakamal@gmail.com>
2012-05-29 16:25:22 +0530

    first run with gae

--- src/application/urls.py
+++ src/application/urls.py
@@ -14,23 +14,24 @@ from application import views
 ## URL dispatch rules
 # App Engine warm up handler
 # See http://code.google.com/appengine/docs/python/config/appconfig.html#Warming_Requests
-app.add_url_rule('/_ah/warmup', 'warmup', view_func=views.warmup)
+#app.add_url_rule('/_ah/warmup', 'warmup', view_func=views.warmup)
 
 # Home page
-app.add_url_rule('/', 'home', view_func=views.home)
+#app.add_url_rule('/', 'home', view_func=views.home)
 
 # Say hello
-app.add_url_rule('/hello/<username>', 'say_hello', view_func=views.say_hello)
+#app.add_url_rule('/hello/<username>', 'say_hello', view_func=views.say_hello)
 
 # Examples list page
-app.add_url_rule('/examples', 'list_examples', view_func=views.list_examples, methods=['GET', 'POST'])
+#app.add_url_rule('/examples', 'list_examples', view_func=views.list_examples, methods=['GET', 'POST'])
 
 # Contrived admin-only view example
-app.add_url_rule('/admin_only', 'admin_only', view_func=views.admin_only)
+#app.add_url_rule('/admin_only', 'admin_only', view_func=views.admin_only)
 
 # Delete an example (post method only)
-app.add_url_rule('/examples/delete/<int:example_id>', view_func=views.delete_example, methods=['POST'])
+#app.add_url_rule('/examples/delete/<int:example_id>', view_func=views.delete_example, methods=['POST'])
 
+app.add_url_rule('/search/<string:search_words>', view_func=views.search, methods = ['GET','POST'])
 
 ## Error handlers
 # Handle 404 errors
